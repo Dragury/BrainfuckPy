04b2f515d7f128fefcafc904945940d877cdd78e
Elliot Hargreaves <Elliot.a.h@outlook.com>
2017-03-12 07:24:26 +0000

    Initial commit

---
+++ src/brainfuck.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+import sys
+
+mem = bytearray(30000)
+pointer = 0
+
+program = ""
+
+
+def process_args():
+    i = 0
+    while i < len(sys.argv):
+        i += 1
+
+
+def run():
+    pass
+
+
+def process_instruction():
+    pass
+
+
+def read_program():
+    pass
+
+
+if __name__ == '__main__':
+    process_args()
+    read_program()
+    run()
+    pass
